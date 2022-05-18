eff08f3a4b6e4174cb175a244850b87a5d973fc5
chandaweia <chandaweia@hotmail.com>
2022-05-18 14:35:32 -0400

    update

--- monitor_cpuusage.py
+++ monitor_cpuusage.py
@@ -21,7 +21,7 @@ def getcpu(pid):
     for i in range(times):
         cpures=p.cpu_percent(interval=1)
         usage+=cpures
-        #print("pid:{0},time:{1},cpu ratio:{2}".format(pid,i,cpures))
+        print("pid:{0},time:{1},cpu ratio:{2}".format(pid,i,cpures))
 
     avgusage=round(usage/times,2)
     print("--------Result--------pid:{0},avg usage:{1}%".format(pid, avgusage))
